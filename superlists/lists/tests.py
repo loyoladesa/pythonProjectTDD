1ec0d9ac952e69ff19ed20f3cddc1c4ecab1c42b
Sidney Loyola <loyoladesa@gmail.com>
2023-09-09 19:27:46 -0300

    Refatoração 'home.html'

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from django.urls import resolve
 from django.http import HttpRequest
+from django.template.loader import render_to_string
 
 from lists.views import *
 
@@ -11,9 +12,13 @@ class HomePageTest(TestCase):
         self.assertEqual(found.func,home_page)
 
     def test_home_page_returns_correct_html(self):
-        request = HttpRequest()
-        response = home_page(request)
+        #request = HttpRequest()
+        response = self.client.get('/')
         html = response.content.decode('utf8')
+        expected_html = render_to_string('home.html')
         self.assertTrue(html.startswith('<html>'))
         self.assertIn('<title>To-Do lists</title>',html)
         self.assertTrue(html.endswith('</html>'))
+
+        self.assertTemplateUsed(response,'home.html')
+        self.assertTemplateUsed(response, 'wrong.html')
