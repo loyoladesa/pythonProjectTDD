f6f98d9ac46e6bbb5052d56f33a812d02d9e97b0
Sidney Loyola <loyoladesa@gmail.com>
2023-09-09 19:28:48 -0300

    Refatoração reduz testes

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -7,18 +7,11 @@ from lists.views import *
 
 # Create your tests here.
 class HomePageTest(TestCase):
-    def test_root_url_resolves_to_home_page_view(self):
-        found = resolve('/')
-        self.assertEqual(found.func,home_page)
+
 
     def test_home_page_returns_correct_html(self):
         #request = HttpRequest()
         response = self.client.get('/')
-        html = response.content.decode('utf8')
-        expected_html = render_to_string('home.html')
-        self.assertTrue(html.startswith('<html>'))
-        self.assertIn('<title>To-Do lists</title>',html)
-        self.assertTrue(html.endswith('</html>'))
 
         self.assertTemplateUsed(response,'home.html')
-        self.assertTemplateUsed(response, 'wrong.html')
+
